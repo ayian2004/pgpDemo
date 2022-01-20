5581da95aa62ec677c8a64b8f76c96c627c79b35
Alkis Yian <alkiviadis.giannakoulias@eurodyn.com>
2022-01-20 16:16:27 +0200

    Update pgpDemo.py

--- pgpDemo.py
+++ pgpDemo.py
@@ -1,50 +1,60 @@
-from pgpy.constants import PubKeyAlgorithm, EllipticCurveOID, KeyFlags, HashAlgorithm, SymmetricKeyAlgorithm, CompressionAlgorithm
 from datetime import timedelta
+from pgpy.constants import PubKeyAlgorithm, EllipticCurveOID, KeyFlags, HashAlgorithm, SymmetricKeyAlgorithm, CompressionAlgorithm
+
 import pgpy
 
-# we can start by generating a primary key. For this example, we'll use RSA, but it could be DSA or ECDSA as well
+# we start by generating a primary key. For this example, we'll use RSA, but it could be DSA or ECDSA as well
 key = pgpy.PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, 4096)
 
 #new user id for the key
-uid = pgpy.PGPUID.new('Spyros Grammatakis', comment='Honest user', email='test@test.com')
+uid = pgpy.PGPUID.new('Alkiviadis Giannakoulias', comment='MTE2105 user', email='alkiviadis.giannakoulias@ssl-unipi.gr')
 
 # add the new user id to the key
-key.add_uid(uid,seflsign=True, usage={KeyFlags.Sign},
-            hashes=[HashAlgorithm.SHA256],
-	    ciphers=[SymmetricKeyAlgorithm.AES256],
+# set the key to expire in 1 year
+key.add_uid(uid, usage={KeyFlags.Sign, KeyFlags.EncryptCommunications, KeyFlags.EncryptStorage},
+            hashes=[HashAlgorithm.SHA256, HashAlgorithm.SHA384, HashAlgorithm.SHA512, HashAlgorithm.SHA224],
+            ciphers=[SymmetricKeyAlgorithm.AES256, SymmetricKeyAlgorithm.AES192, SymmetricKeyAlgorithm.AES128],
             compression=[CompressionAlgorithm.ZLIB, CompressionAlgorithm.BZ2, CompressionAlgorithm.ZIP, CompressionAlgorithm.Uncompressed],
-	    key_expires=timedelta(days=365))
-
-# protect primary private key with passphrase
-key.protect("primary", SymmetricKeyAlgorithm.AES256, HashAlgorithm.SHA256)
+            key_expires=timedelta(days=365))
 
 # generate a sub key.
 subkey = pgpy.PGPKey.new(PubKeyAlgorithm.ECDH, EllipticCurveOID.NIST_P256)
 
 
+# protect primary private key with passphrase
+key.protect("MT32I0S_P@s$PhrAs3_pkey", SymmetricKeyAlgorithm.AES256, HashAlgorithm.SHA256)
 # protect subkey private key with passphraee
-subkey.protect("sub", SymmetricKeyAlgorithm.AES256, HashAlgorithm.SHA256)
+subkey.protect("MT32I0S_P@s$PhrAs3_skey", SymmetricKeyAlgorithm.AES256, HashAlgorithm.SHA256)
 
-# compressed by default with ZIP DEFLATE
-message = pgpy.PGPMessage.new("This is the new message!")
+# Ask user for message to encrypt
+message = input("Enter your message to encrypt with PGP: ")
+# compress message by default with ZIP DEFLATE
+message = pgpy.PGPMessage.new(message)
 
-#sign key and message
-with key.unlock("primary"):
-	sig = key.sign(message)
+# Encrypt message
+# since the key is protected with a passphrase, we will need to unlock it first
+with key.unlock("MT32I0S_P@s$PhrAs3_pkey"):
+# the bitwise OR operator '|' is used to add a signature to a PGPMessage.
 	message |= key.sign(message)
-	with subkey.unlock("sub"):
+# since the subkey is protected with a passphrase, we will need to unlock it first
+	with subkey.unlock("MT32I0S_P@s$PhrAs3_skey"):
 		key.add_subkey(subkey, usage={KeyFlags.EncryptCommunications})
 		subkey.pubkey |= key.certify(subkey.pubkey)
-		key.protect("key", SymmetricKeyAlgorithm.AES256, HashAlgorithm.SHA256)
-
+		key.protect("new_P@s$PhrAs3_skey", SymmetricKeyAlgorithm.AES256, HashAlgorithm.SHA256)
+        
 encrypted_message = subkey.pubkey.encrypt(message)
-print(encrypted_message)
+print(f"\n\nEncrypted message: {encrypted_message}\n")
+
+# Although encryoting  things uses multiple methods, there is only one method to remember for verifying
 key.pubkey.verify(message)
-with key.unlock("key"):
-	assert subkey.is_protected
-	with subkey.unlock("key"):
+
+
+# Decrypt message
+with key.unlock("new_P@s$PhrAs3_skey"):
+	assert key.is_protected
+	with subkey.unlock("new_P@s$PhrAs3_skey"):
 		assert subkey.is_unlocked
-		print(message)
 		decrypted_message = subkey.decrypt(encrypted_message)
-		print(key.pubkey.verify(decrypted_message))
-		print(decrypted_message.message)
+        
+print(f"\n\nDecrypted message verify signature: {key.pubkey.verify(decrypted_message)}")
+print(f"\n\nDecrypted message: {decrypted_message.message}\n")
